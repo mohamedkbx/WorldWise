d7390f45ce52a0b130a44f9812773f76fafeb4fd
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2025-01-06 20:13:43 +0200

    feat: setup routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import HomePage from "./pages/HomePage";
+
 export default function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="product" element={<Product />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
