3458336878f04ac1f0c2740afffebdd00d153bb9
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2025-01-08 16:19:43 +0200

    feat: set style layout for AppLayout

---
+++ src/components/Footer.jsx
@@ -0,0 +1,5 @@
+import styles from "./Footer.module.css";
+
+<footer className={styles.footer}>
+  <p className={styles.copyright}>&copy; Copyright {new Date().getFullYear()} by Worldwise Inc</p>
+</footer>;
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,10 +1,10 @@
-import AppNav from "../components/AppNav";
+import Sidebar from "../components/Sidebar";
+import styles from "./AppLayout.module.css";
 
 export default function AppLayout() {
   return (
-    <div>
-      <AppNav />
-      APP
+    <div className={styles.app}>
+      <Sidebar />
     </div>
   );
 }
