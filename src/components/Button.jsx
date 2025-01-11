8a0e6b4b1b5effd8d221668128fde1e1c1b5e212
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2025-01-11 03:47:04 +0200

    feat: handle form component with navigate route

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,8 @@
+import styles from "./Button.module.css";
 export default function Button({ children, onClick, type }) {
-  return <button onClick={onClick}>{children}</button>;
+  return (
+    <button onClick={onClick} className={`${styles.btn} ${styles[type]}`}>
+      {children}
+    </button>
+  );
 }
