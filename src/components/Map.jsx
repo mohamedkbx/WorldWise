bec18092470b3f372efec0d09e5a606bf05fa037
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2025-01-18 02:45:35 +0200

    feat: displaying city markers on the map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,11 +2,15 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import { useState } from "react";
+import { useCities } from "../context/CitiesProvider";
 export default function Map() {
   const [searchParams, setSearchParams] = useSearchParams();
   const [mapPostion, setMapPosition] = useState({ lat: 40, lng: 0 });
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
+
+  const { cities } = useCities();
+
 
   // const navigate = useNavigate();
 
@@ -17,17 +21,28 @@ export default function Map() {
       //   navigate("form");
       // }}
     >
-     <MapContainer center={mapPostion} zoom={13} scrollWheelZoom={true} className={styles.map}>
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
-    />
-    <Marker position={mapPostion}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
-  </MapContainer>
+      <MapContainer
+        center={mapPostion}
+        zoom={13}
+        scrollWheelZoom={true}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        {cities.map((city) => (
+          <Marker
+            position={[city.position?.lat, city.position?.lng]}
+            key={city.id}
+          >
+            <Popup>
+             
+             <span>{city.emoji}</span><span> </span><span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
     </div>
   );
 }
