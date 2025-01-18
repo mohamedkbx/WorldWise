6379896aaedfcfa8e402633f125e42bb52dec794
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2025-01-18 02:21:30 +0200

    feat: add the map component

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,20 +1,33 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { useState } from "react";
 export default function Map() {
   const [searchParams, setSearchParams] = useSearchParams();
+  const [mapPostion, setMapPosition] = useState({ lat: 40, lng: 0 });
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
 
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   return (
     <div
       className={styles.mapContainer}
-      onClick={() => {
-        navigate("form");
-      }}
+      // onClick={() => {
+      //   navigate("form");
+      // }}
     >
-      Map
+     <MapContainer center={mapPostion} zoom={13} scrollWheelZoom={true} className={styles.map}>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+    />
+    <Marker position={mapPostion}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+  </MapContainer>
     </div>
   );
 }
