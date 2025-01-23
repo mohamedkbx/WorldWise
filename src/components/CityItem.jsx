9cf587a7ab9e905a4ae973230361019798b74f09
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2025-01-23 11:16:32 +0200

    feat: delet a city functionality

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -12,21 +12,26 @@ const formatDate = (date) =>
     weekday: "long",
   }).format(new Date(date));
 export default function CityItem({ city }) {
-  const { currentCity } = useCities();
+  const { currentCity, deleteCity } = useCities();
   const { cityName, emoji, date, id, position } = city;
+
+  function handleClick(e) {
+   e.preventDefault();
+  deleteCity(id)
+  }
   return (
     
       <li>
         <Link
           to={`${id}?lat=${position.lat}&lng=${position.lng}`}
-          className={`${styles.cityItem} ${
-            id === currentCity.id ? styles["cityItem--active"] : ""
-          }`}
+            className={`${styles.cityItem} ${
+              id === currentCity.id ? styles["cityItem--active"] : ""
+            }`}
         >
           <span className={styles.emoji}>{emoji}</span>
           <h3 className={styles.name}> {cityName}</h3>
           <time className={styles.date}>{formatDate(date)}</time>
-          <button className={styles.deleteBtn}>&times;</button>
+          <button className={styles.deleteBtn} onClick={handleClick}>&times;</button>
         </Link>
       </li>
       
--- src/context/CitiesProvider.jsx
+++ src/context/CitiesProvider.jsx
@@ -57,8 +57,22 @@ function CitiesProvider({children}) {
       }
 
   }
+  async function deleteCity(id) {
+    try {
+        setIsLoading(true);
+      await fetch(`${BASE_URL}/cities/${id}`,{
+          method:"DELETE",      
+        });
+        setCities((cities)=> cities.filter((city)=> city.id!==id ));
+      } catch {
+        console.log("There was an error deleting city");
+      } finally {
+        setIsLoading(false);
+      }
+
+  }
   return (
-    <CitiesContext.Provider value={{ cities, isloading, currentCity,getCity,createCity }}>
+    <CitiesContext.Provider value={{ cities, isloading, currentCity,getCity,createCity,deleteCity }}>
         {children}
     </CitiesContext.Provider>
   )
